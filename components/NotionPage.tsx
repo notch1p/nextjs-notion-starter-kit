11397b8084b46c1f791f71001f4d5e90388ee5ef
Evan Gao <notch1p2004@outlook.com>
2023-07-09 21:20:40 +0800

    fix c++ & f# syntax highlight

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -288,6 +288,25 @@ export const NotionPage: React.FC<types.PageProps> = ({
     getPageProperty<string>('Description', block, recordMap) ||
     config.description
 
+  try {
+    Object.keys(recordMap.block).forEach((key) => {
+      try {
+        if (recordMap.block[key].value.properties.language[0][0] === 'C++') {
+          recordMap.block[key].value.properties.language[0][0] = 'Cpp'
+        } else if (
+          recordMap.block[key].value.properties.language[0][0] === 'F#'
+        ) {
+          recordMap.block[key].value.properties.language[0][0] = 'Fsharp'
+        }
+        console.log(recordMap.block[key].value)
+      } catch (_) {
+        /* */
+      }
+    })
+  } catch (_) {
+    /* */
+  }
+
   return (
     <>
       <PageHead
