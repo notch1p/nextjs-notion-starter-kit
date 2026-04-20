8db5af75da0d191f9f0930e7f6e30c621cdbaa00
Evan Gao <notch1p2004@outlook.com>
2026-04-20 18:36:15 +0800

    fix prismjs display issue on bright mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 // used for code syntax highlighting (optional)
-//import 'prismjs/themes/prism-coy.css'
+import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/styles.css'
 import 'styles/global.css'
 // this might be better for dark mode
-import 'prismjs/themes/prism-okaidia.css'
+//import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
